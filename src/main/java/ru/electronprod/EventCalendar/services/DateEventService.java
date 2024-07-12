5fb5a20cd9e8a500b66d218c0f496f9dbe63535d
Electron_prod <80621922+Electronprod@users.noreply.github.com>
2024-07-12 15:16:40 +0300

    added report generation function

--- src/main/java/ru/electronprod/EventCalendar/services/DateEventService.java
+++ src/main/java/ru/electronprod/EventCalendar/services/DateEventService.java
@@ -1,9 +1,12 @@
 package ru.electronprod.EventCalendar.services;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.electronprod.EventCalendar.models.DateEvent;
 import ru.electronprod.EventCalendar.repositories.DateEventRepository;
@@ -13,9 +16,9 @@ public class DateEventService {
 	@Autowired
 	private DateEventRepository rep;
 
-	public void addDateEvent(DateEvent e) throws Exception {
+	public void addDateEvent(DateEvent e) throws NullPointerException {
 		if (e == null) {
-			throw new Exception("Error adding: DateEvent=null");
+			throw new NullPointerException("Error adding: DateEvent=null");
 		}
 		rep.save(e);
 	}
@@ -23,8 +26,7 @@ public class DateEventService {
 	public JSONArray getDayEvents(String date) {
 		JSONArray arr = new JSONArray();
 		List<DateEvent> events = rep.findAllByDate(date);
-		for (int i = 0; i < events.size(); i++) {
-			DateEvent event = events.get(i);
+		for (DateEvent event : events) {
 			JSONObject obj = new JSONObject();
 			obj.put("content", event.getContent());
 			obj.put("by", event.getName());
@@ -37,5 +39,24 @@ public class DateEventService {
 			arr.add(obj);
 		}
 		return arr;
+	}
+
+	// Bad
+	public String generateReport() {
+		List<DateEvent> events = rep.findAll();
+		Collections.sort(events, new DateEventComparator());
+		String result = "<head><title>Data Report</title><meta charset=\"UTF-8\"><style type=\"text/css\">\r\n"
+				+ "      TABLE {\r\n" + "       width: 500px; /* Ширина таблицы */\r\n"
+				+ "       border-collapse: collapse; /* Убираем двойные линии между ячейками */\r\n" + "      }\r\n"
+				+ "      TD, TH {\r\n" + "       padding: 3px; /* Поля вокруг содержимого таблицы */\r\n"
+				+ "       border: 1px solid black; /* Параметры рамки */\r\n" + "      }\r\n" + "      TH {\r\n"
+				+ "       background: #b0e0e6; /* Цвет фона */\r\n" + "      }\r\n" + "     </style></head><table>\r\n"
+				+ "    <tr>\r\n" + "        <th>Дата</th>\r\n" + "        <th>Автор</th>\r\n"
+				+ "        <th>Контент</th>\r\n" + "    </tr>";
+		for (DateEvent e : events) {
+			result = result + "<tr><td>" + e.getDate() + "</td><td>" + e.getName() + "</td><td>" + e.getContent()
+					+ "</td></tr>";
+		}
+		return result + "</table>";
 	}
 }
