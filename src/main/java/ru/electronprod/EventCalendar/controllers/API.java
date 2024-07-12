51a8d2ad180b31869eb7ed2158d8b72874f3bc06
Electron_prod <80621922+Electronprod@users.noreply.github.com>
2024-07-12 15:16:40 +0300

    added report generation function

--- src/main/java/ru/electronprod/EventCalendar/controllers/API.java
+++ src/main/java/ru/electronprod/EventCalendar/controllers/API.java
@@ -1,9 +1,12 @@
 package ru.electronprod.EventCalendar.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import ru.electronprod.EventCalendar.services.DateEventService;
 
@@ -11,9 +14,21 @@ import ru.electronprod.EventCalendar.services.DateEventService;
 public class API {
 	@Autowired
 	private DateEventService des;
+	@Value("${report.secretkey}")
+	private String secret;
 
 	@GetMapping("/daydata")
 	public String getDayData(@RequestParam String day) {
 		return des.getDayEvents(day).toJSONString();
+	}
+
+	@GetMapping("/report")
+	public String report(@RequestParam("key") String key) {
+		// Checking key
+		if (!key.equals(secret)) {
+			// throw 401 error
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Incorrect key.");
+		}
+		return des.generateReport();
 	}
 }
