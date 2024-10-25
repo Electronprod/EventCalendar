65014a27b63a04dd598313cee43ea4cf76514a72
Electron_prod <80621922+Electronprod@users.noreply.github.com>
2024-10-25 13:27:12 +0300

    Big remake: step 2 - change db to SQLite

--- src/main/java/ru/electronprod/EventCalendar/models/Event.java
+++ src/main/java/ru/electronprod/EventCalendar/models/Event.java
@@ -3,6 +3,7 @@ package ru.electronprod.EventCalendar.models;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Event {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
